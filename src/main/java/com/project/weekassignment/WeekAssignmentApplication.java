575ceec55b4f83ff8663914f763d8fcf0289a0b9
DAN-MU-ZI <39262980+DAN-MU-ZI@users.noreply.github.com>
2024-04-28 21:38:49 +0900

    feat - 프로젝트 초기화

---
+++ src/main/java/com/project/weekassignment/WeekAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.project.weekassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeekAssignmentApplication.class, args);
+	}
+
+}
