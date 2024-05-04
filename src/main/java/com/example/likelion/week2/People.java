ba88c77269acf23b63bdfcfede11ce694b9737a3
juju <jujunoidea@gmail.com>
2024-05-04 16:16:25 +0900

    feat: 객체 100개 만들기

---
+++ src/main/java/com/example/likelion/week2/People.java
@@ -0,0 +1,11 @@
+package com.example.likelion.week2;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class People {
+
+    private final String name;
+    private final int age;
+
+}
