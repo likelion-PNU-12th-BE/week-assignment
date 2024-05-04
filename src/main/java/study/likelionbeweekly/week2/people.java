74a4fdf4ff77ae781465d753e7d63c194efbee0d
Karryun <165464282+Karryun@users.noreply.github.com>
2024-05-04 16:15:01 +0900

    feat: 객체 100개 만들어보

---
+++ src/main/java/study/likelionbeweekly/week2/people.java
@@ -0,0 +1,10 @@
+package study.likelionbeweekly.week2;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class people {
+    private final String name;
+    private final int age;
+
+}
