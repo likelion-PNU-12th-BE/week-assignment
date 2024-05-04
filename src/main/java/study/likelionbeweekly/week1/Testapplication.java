f2b977e58ee6c26f9e34e7b344453b2c611920da
Karryun <165464282+Karryun@users.noreply.github.com>
2024-05-04 15:41:29 +0900

    init: project
    초기화

---
+++ src/main/java/study/likelionbeweekly/week1/Testapplication.java
@@ -0,0 +1,12 @@
+package study.likelionbeweekly.week1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testapplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Testapplication.class, args);
+    }
+}
