5b2a598139aa074fb2e0bd0ce058cb8b14773430
donghyun kim <gimdonghyun5434@gmail.com>
2024-05-08 15:54:44 +0900

    2주차 과제 제출

---
+++ week2/core/src/main/java/practice/core/DependentObject1.java
@@ -0,0 +1,8 @@
+package practice.core;
+
+import org.springframework.stereotype.Component;
+
+@Component("DependentObject1")
+public class DependentObject1 {
+
+}
---
+++ week2/core/src/main/java/practice/core/DependentObject2.java
@@ -0,0 +1,8 @@
+package practice.core;
+
+import org.springframework.stereotype.Component;
+
+@Component("DependentObject2")
+public class DependentObject2 {
+
+}
