7e96418064aae262b78720399f515f1b9a4000b6
juju <jujunoidea@gmail.com>
2024-05-09 21:37:43 +0900

    feat:의존성 주입

--- src/main/java/com/example/likelion/week2/spring/DefaultService.java
+++
@@ -1,4 +0,0 @@
-package com.example.likelion.week2.spring;
-
-public class DefaultService {
-}
---
+++ src/main/java/study/likelionbeweekly/week2/ObjectExample.java
@@ -0,0 +1,14 @@
+package study.likelionbeweekly.week2;
+
+public class ObjectExample {
+
+    public static void main(String[] args) {
+        People people = new People("박주은", 25);
+        
+        for (int i = 0; i < 100; i++) {
+            People people1 = new People("박주은", 25);
+            System.out.println("people1 = " + people1);
+        }
+        
+    }
+}
