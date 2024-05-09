d33c8deff17408ffd31770259a34a283c9c62980
mo0n_white <ohyerin02@naver.com>
2024-05-09 23:42:05 +0900

    feat: bean 생성 with component annotation

---
+++ src/main/java/study/likelionBEweekly/week2/spring/CreateBean02.java
@@ -0,0 +1,10 @@
+package study.likelionBEweekly.week2.spring;
+
+import org.springframework.stereotype.Component;
+
+public class CreateBean02 {
+    @Component
+    public class Book {
+        //
+    }
+}
