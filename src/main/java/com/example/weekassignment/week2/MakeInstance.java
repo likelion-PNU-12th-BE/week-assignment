d77884995b2a27376fdebd034181e39d54ef1fd2
h-hye <euneng356@gmail.com>
2024-05-09 19:20:16 +0900

    임은혜 2주차 과제 제출합니다!
    2주차 과제 제출합니다!

---
+++ src/main/java/com/example/weekassignment/week2/MakeInstance.java
@@ -0,0 +1,13 @@
+package com.example.weekassignment.week2;
+
+public class MakeInstance {
+    public static void main(String[] args) {
+        //인스턴스 100개 만들기
+        for (int i= 0;  i<100 ; i++) {
+            MakeInstance person = new MakeInstance();
+            System.out.println(person);
+        }
+    }
+}
+
+//인스턴스를 100개 생성하면 각각 주소가 다 다름. 부하가 많이 발생 > 싱글톤 사용하는 거어
