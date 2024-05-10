781f750361c82e095f5f42531295969f4f12a9ae
h-hye <euneng356@gmail.com>
2024-05-11 02:01:26 +0900

    MVC 패턴 적용해보기
    OutputView 부분에 에러가 발생했는데 디버깅을 아직 못했다.

---
+++ src/main/java/com/example/weekassignment/week3/OutputView.java
@@ -0,0 +1,12 @@
+package com.example.weekassignment.week3;
+
+public class OutputView {
+    public void printProfile(Student student){
+        System.out.println(
+                "내 이름은 " + student.getName() + "입니다."
+        );
+        System.out.println(
+                "내 나이는 " + student.getAge() + "입니다."
+        );
+    }
+}
---
+++ src/main/java/com/example/weekassignment/week3/controller.java
@@ -0,0 +1,8 @@
+package com.example.weekassignment.week3;
+
+public class controller {
+    public static void main(String[] args) {
+        Student student = new Student("기철", 25);
+        //OutputView.printProfile(student);
+    }
+}
