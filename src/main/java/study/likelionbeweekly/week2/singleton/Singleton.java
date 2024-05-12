dd1c89bfa35e57076ee9935608cbe576d91b25d1
MinseokGo <rhalstjr1999@naver.com>
2024-05-12 18:37:49 +0900

    refactor: get singleton instance logic

--- src/main/java/study/likelionbeweekly/week2/singleton/Singleton.java
+++ src/main/java/study/likelionbeweekly/week2/singleton/Singleton.java
@@ -1,13 +1,18 @@
 package study.likelionbeweekly.week2.singleton;
 
+import java.util.Objects;
+
 public class Singleton {
 
-    private static final Singleton INSTANCE = new Singleton();
+    private static Singleton INSTANCE;
 
     private Singleton() {
     }
 
     public static Singleton getInstance() {
+        if (Objects.isNull(INSTANCE)) {
+            INSTANCE = new Singleton();
+        }
         return INSTANCE;
     }
 }
