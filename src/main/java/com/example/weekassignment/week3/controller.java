675dde755c08ddcc4a0b143885bb7cc08df29df3
h-hye <euneng356@gmail.com>
2024-05-15 21:58:51 +0900

    3주차 과제 제출합니다!
    study3

--- src/main/java/com/example/weekassignment/week3/controller.java
+++
@@ -1,8 +0,0 @@
-package com.example.weekassignment.week3;
-
-public class controller {
-    public static void main(String[] args) {
-        Student student = new Student("기철", 25);
-        //OutputView.printProfile(student);
-    }
-}
