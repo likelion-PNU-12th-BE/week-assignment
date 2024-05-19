32942f0fa5fbb5c2800ab4d184d7e0f40a24f4f9
Hanjjong <tim4974@naver.com>
2024-05-19 23:33:57 +0900

    [refactor] : modify class name

--- src/main/java/study/likelionbeweekly/LikelionbeWeeklyApplication.java
+++ src/main/java/study/likelionbeweekly/LikelionbeWeeklyApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LikelionBeWeeklyApplication {
+public class LikelionbeWeeklyApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LikelionBeWeeklyApplication.class, args);
+		SpringApplication.run(LikelionbeWeeklyApplication.class, args);
 	}
 
 }
