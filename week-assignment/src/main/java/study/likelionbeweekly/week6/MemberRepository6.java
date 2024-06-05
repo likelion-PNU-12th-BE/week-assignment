a94438d33f43dded16b44a0675c9f9063e6ec071
JongHan Jeon <moohan211@naver.com>
2024-06-05 20:49:06 +0900

    feat: Repository 작성

---
+++ week-assignment/src/main/java/study/likelionbeweekly/week6/MemberRepository6.java
@@ -0,0 +1,7 @@
+package study.likelionbeweekly.week6;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import study.likelionbeweekly.week6.entity.Member;
+
+public interface MemberRepository6 extends JpaRepository<Member, Long> {
+}
